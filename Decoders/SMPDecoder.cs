98897240e13b0a40f4f21d39b5f4358af6f8f4aa
Peter Kremsner <peterkremsner@gmx.at>
2021-04-02 14:59:20 +0200

    SmpDecoder implementation

---
+++ Decoders/SMPDecoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using libsmp;
+
+namespace libconnection.Decoders
+{
+    public class SMPDecoder
+    {
+        private ManagedSMP smp = new ManagedSMP();
+
+        public SMPDecoder()
+        {
+
+        }
+    }
+}
